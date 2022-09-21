10025562a0d2b7743eb170c5516c88ab01938e69
probatorem-h1 <hackermanresurrected@gmail.com>
2022-09-21 15:35:59 -0500

    commit

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -44,18 +44,6 @@ library Roles {
     }
 }
 
-library Array {
-    function removeElement(uint256[] storage _array, uint256 _element) public {
-        for (uint256 i; i < _array.length; i++) {
-            if (_array[i] == _element) {
-                _array[i] = _array[_array.length - 1];
-                _array.pop();
-                break;
-            }
-        }
-    }
-}
-
 contract Marketplace {
     uint256[] public activeListings;
     uint256[] public closedListings;
